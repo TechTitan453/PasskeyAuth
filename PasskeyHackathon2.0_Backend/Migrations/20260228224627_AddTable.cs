2de0b7f1ee28f7f71acd7be7bb54f9c9b0a6feea
Aniket Joshi <aniketjoshi056@gmail.com>
2026-03-01 10:08:53 +0530

    Add migrations for PasskeyCredentials and PasskeyChallenges tables

---
+++ PasskeyHackathon2.0_Backend/Migrations/20260228224627_AddTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PasskeyHackathon2._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasskeyCredentials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CredentialId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicKey = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SignatureCounter = table.Column<long>(type: "bigint", nullable: false),
+                    CredType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fingerprint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Domain = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasskeyCredentials", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasskeyCredentials");
+        }
+    }
+}
